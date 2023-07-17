1eddee59dd111d8185ae1e19714189b1af5a0c6b
keaksey <keaksey.sum@gmail.com>
2023-07-17 23:40:40 +0700

    feat: fix checkout and contact us page

--- saleor/graphql/account/mutations/base.py
+++ saleor/graphql/account/mutations/base.py
@@ -295,6 +295,9 @@ class PasswordChange(BaseMutation):
     user = graphene.Field(User, description="A user instance with a new password.")
 
     class Arguments:
+        customer_id = graphene.ID(
+            required=False, description="ID of a customer to change the password for."
+        )
         old_password = graphene.String(
             required=False, description="Current user password."
         )
@@ -324,22 +327,27 @@ class PasswordChange(BaseMutation):
         user = cast(models.User, user)
         old_password = data.get("old_password")
         new_password = data["new_password"]
+        customer_id = data.get("customer_id")
+        
+        if not user.is_superuser:
+            if old_password is None:
+                # Spend time hashing useless password
+                # This prevents the outside actors from telling if user has
+                # unusable password set or not by measuring API's response time
+                make_password("waste-time")
 
-        if old_password is None:
-            # Spend time hashing useless password
-            # This prevents the outside actors from telling if user has
-            # unusable password set or not by measuring API's response time
-            make_password("waste-time")
-
-            if user.has_usable_password():
+                if user.has_usable_password():
+                    cls.raise_invalid_credentials()
+            elif not user.check_password(old_password):
                 cls.raise_invalid_credentials()
-        elif not user.check_password(old_password):
-            cls.raise_invalid_credentials()
         try:
             password_validation.validate_password(new_password, user)
         except ValidationError as error:
             raise ValidationError({"new_password": error})
-
+        
+        if customer_id:
+            user = cls.get_node_or_error(info, customer_id, only_type=User)
+            
         user.set_password(new_password)
         user.save(update_fields=["password", "updated_at"])
         account_events.customer_password_changed_event(user=user)
